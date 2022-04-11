9299870b675998c696d59ca9ba799161ee5c3c65
farhandardiri <faran.f4124n@gmail.com>
2022-04-12 01:55:23 +0700

    edit beberapa css shudow clas dan element

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+      },
+      fontFamily: {
+        inter: ['Inter']
+      },
+      colors: {
+        imut: '#bada55',
+        kopi: '#c0ffee'
+      }
+    },
+  },
+  plugins: [],
+}
